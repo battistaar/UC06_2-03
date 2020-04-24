5a900a64054ac5b42adf69c37076ab320398817d
Enrico Battistella <enrico.battistella@digitalrockers.it>
2020-04-24 12:28:35 +0200

    fix id

--- src/pages/book/book.controller.js
+++ src/pages/book/book.controller.js
@@ -27,7 +27,6 @@ module.exports.addPage = (req, res) => {
 
 module.exports.addBook = (req, res, next) => {
     const newBook = {
-        id: '' + allBooks.length,
         title: req.body.title,
         authors: [req.body.author],
         description: req.body.description,
